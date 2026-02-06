81f8faa67166c477a36a2bb4d7bdc6386b92f8fc
d4970-sltung <d4970.sltung@gmail.com>
2026-02-06 12:37:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // TODO: Update this if your repository name is different
+  base: '/tua/', // TODO: Update this if your repository name is different
 })
